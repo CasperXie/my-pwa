5e2d9cee91f1f02b225407ebdcb6c4dca1f29a7b
CasperXie <46865116+CasperXie@users.noreply.github.com>
2025-10-24 20:19:18 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,23 +2,18 @@ const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './style.css'
-  // 可以把你的 JS 或其他資源也加進來
+  './manifest.json'
+  // 若有其他資源可加進來
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
